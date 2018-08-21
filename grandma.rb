84b1bb51e073e86ebceaff71b19fa6182826747c
William Lathrop <wlathrop10@gmail.com>
2018-08-21 17:42:54 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,5 @@
 def speak_to_grandma(phrase)
-
-    if phrase == "I LOVE YOU GRANDMA!"
+  if phrase == "I LOVE YOU GRANDMA!"
       return "I LOVE YOU TOO PUMPKIN!"
     
     elsif phrase == phrase.downcase
