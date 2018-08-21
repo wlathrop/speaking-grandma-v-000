8498ebf2efad62b11f2451e02c2eb7343337bcdf
William Lathrop <wlathrop10@gmail.com>
2018-08-21 17:48:37 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,11 +1,10 @@
 def speak_to_grandma(phrase)
-  if(phrase=="I LOVE YOU GRANDMA!")
-    return "I LOVE YOU TOO PUMPKIN!"
-  elsif (phrase=="#{phrase}".upcase)
-    return "NO, NOT SINCE 1938!"
-  else
-    return "HUH?! SPEAK UP, SONNY!"
+   if(phrase== "I LOVE YOU GRANDMA!")
+     return "ILOVE YOU TOO PUMPKIN!"
+    elsif (phrase=="#{phrase}".upcase)
+      return "NO, NOT SINCE 1938!"
+    else
+      return "HUH?! SPEAK UP, SONNY!"
+    end
   end
-end
-
- 
+  
